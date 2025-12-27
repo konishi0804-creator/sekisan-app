744500e5a4551df54aae37df557edfe7dde205d4
konishi0804-creator <h.konishi.0804@gmail.com>
2025-12-27 14:55:41 +0900

    Update next.config.ts to ignore canvas/pdfjs-dist in server build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // @ts-ignore - serverExternalPackages is available in newer Next.js versions but types might lag
+  serverExternalPackages: ["pdfjs-dist"],
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    // Fix for pdfjs-dist 4.x+ standard font packaging
+    config.resolve.alias.encoding = false;
+    return config;
+  },
 };
 
 export default nextConfig;
